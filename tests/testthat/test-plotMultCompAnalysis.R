7233635bb65a24497c2154319b4b6b967d57f549
Izumi Ando <andoizumita@gmail.com>
2024-11-29 16:29:47 -0500

    want to see vignette

--- tests/testthat/test-plotMultCompAnalysis.R
+++ tests/testthat/test-plotMultCompAnalysis.R
@@ -1,8 +1,20 @@
-# # test_that("multiplication works", {
-# #   expect_equal(2 * 2, 4)
-# # })
-#
-# # Things to test
+## Input catching
+### 1) catches when featureMatrix is not a data frame
+### 2) creates groups data frame when groups is NULL
+### 3) catches when groups is not a data frame
+### 4) catches when groups has more than 1 column
+### 5) creates a new directory if the assigned one does not already exist
+
+## Output
+### 1) creates default jpg image in the default directory
+### 2) creates default jpg image in the assigned directory
+### 3) creates assigned jpg image in the default directory
+### 4) creates assigned jpg image in the assigned directory
+### 5) returns a list (i think)
+### 6) does not save image when saveTo is null
+
+
+
 # library(BacPoSSR)
 #
 # test_that("plotMultCompAnalysis works as expected", {
--- vignettes/Tutorial_BacPoSSR.R
+++ vignettes/Tutorial_BacPoSSR.R
@@ -5,18 +5,17 @@ knitr::opts_chunk$set(
 )
 options(repos = c(CRAN = "https://cran.r-project.org"))
 
-## ----setup--------------------------------------------------------------------
-library(BacPoSSR)
-
 ## -----------------------------------------------------------------------------
-install.packages("devtools")
-library("devtools")
-devtools::install_github("izumiando/BacPoSSR", build_vignettes = TRUE)
-library("BacPoSSR")
+# install.packages("devtools")
+# library("devtools")
+# devtools::install_github("izumiando/BacPoSSR", build_vignettes = TRUE)
+# library("BacPoSSR")
 
 ## -----------------------------------------------------------------------------
 ls("package:BacPoSSR")
 
 ## -----------------------------------------------------------------------------
-data(package = "BacPoSSR")
+# replace "file.csv" with the file you want to load
+# filePath <- system.file("extdata", "file.csv", package = "BacPoSSR")
+# loadedData <- read.csv(filePath, header = TRUE)
 
